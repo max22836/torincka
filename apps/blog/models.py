a1d1f434ef861f30a6f0e17dec25cdc9bc477fe2
max22836 <maximnikolau@gmail.com>
2022-07-21 16:21:58 +0300

    init django

---
+++ apps/blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=225)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория блога'
+        verbose_name_plural = 'Категории блога'
+
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=225)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    category = models.ForeignKey(
+        to=BlogCategory,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    tags = models.ManyToManyField(to=Tag, verbose_name='Тэги', blank=True)
+    title = models.CharField(verbose_name='Заголовок', max_lengthо=225)
+    text_preview = models.TextField(verbose_name='Текст-превью')
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата редактирования', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
